5a28759aee6b5b193c6ffd2a2cd7212ecf97cf5d
HasanMt12 <hasan.2mahmud22gmail.com>
2022-11-09 02:49:38 +0600

    data get by mongodb

--- index.js
+++ index.js
@@ -12,11 +12,25 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gniuvqv.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        const myServiceCollection = client.db('serviceDb').collection('services');
+         app.get('/services', async (req, res) => {
+             const query = {}
+             const cursor = myServiceCollection.find(query);
+             const myServices = await cursor.toArray();
+             res.send(myServices);
+         });
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
